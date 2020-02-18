5c52ae7c834959b8d07e0d029463caa80781d79c
Farhan Irani <farhanirani007@gmail.com>
2020-02-19 00:48:53 +0530

    insertion sort

--- Algo-Web.js
+++ Algo-Web.js
@@ -1,23 +1,44 @@
 function bubblesort(){
     document.getElementById("desc-main").innerHTML=`<pre><xmp>
-void bubblesort(int a[], int length)
-{
-    int i,j,temp,flag;
-    for(i=0;i<length-1;i++)
+    void bubblesort(int a[], int length)
     {
-        flag=1;
-        for(j=0;j<length-i-1;j++)
+        int i,j,temp,flag;
+        for(i=0;i<length-1;i++)
         {
-            if(a[j] > a[j+1])
+            flag=1;
+            for(j=0;j<length-i-1;j++)
             {
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-                flag=0;
+                if(a[j] > a[j+1])
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    flag=0;
+                }
+                if(flag==1) break;
             }
-            if(flag==1) break;
         }
     }
-}
    </xmp></pre>`       
-}
+};
+
+function insertionsort(){
+    document.getElementById("desc-main").innerHTML=`<pre><xmp>
+    void insertionsort(int a[], int length)
+    {
+        int i,j,val;
+        for(i=1;i<length;i++)
+        {
+            val=a[i];
+            for(j=i-1;j>=0;j--)
+                if(val<a[j])
+                    a[j+1]=a[j];
+    
+            a[j+1]=val;
+        }
+    }
+    
+   </xmp></pre>`       
+};
+
+
