d8372547df3afb3d094f40bd7fb9c912216e4d20
Farhan Irani <farhanirani007@gmail.com>
2020-02-19 00:19:16 +0530

    added the first proper function :)))

--- Algo-Web.js
+++ Algo-Web.js
@@ -1,38 +1,20 @@
 function bubblesort(){
     document.getElementById("desc-main").innerHTML=`<pre><xmp>
-    #include <stdio.h>
-
-    int main()
-    {
-      int array[100], n, c, d, swap;
-    
-      printf("Enter number of elements\\n");
-      scanf("%d", &n);
-    
-      printf("Enter %d integers\\n", n);
-    
-      for (c = 0; c < n; c++)
-        scanf("%d", &array[c]);
-    
-      for (c = 0 ; c < n - 1; c++)
-      {
-        for (d = 0 ; d < n - c - 1; d++)
-        {
-          if (array[d] > array[d+1]) /* For decreasing order use < */
-          {
-            swap       = array[d];
-            array[d]   = array[d+1];
-            array[d+1] = swap;
-          }
+void bubblesort(int a[], int length)
+{
+    int i,j,temp,flag;
+    for(i=0;i<length-1;i++){
+        flag=1;
+        for(j=0;j<length-i-1;j++){
+            if(a[j] > a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+                flag=0;
+            }
+            if(flag==1) break;
         }
-      }
-    
-      printf("Sorted list in ascending order:\\n");
-    
-      for (c = 0; c < n; c++)
-         printf("%d\\n", array[c]);
-    
-      return 0;
     }
+}
    </xmp></pre>`       
 }
