35520462c448ea161ad185761e29ac73eb5488da
Farhan Irani <farhanirani007@gmail.com>
2020-02-19 10:19:56 +0530

    ins

--- Algo-Web.js
+++ Algo-Web.js
@@ -33,6 +33,8 @@ function insertionsort(){
             for(j=i-1;j>=0;j--)
                 if(val<a[j])
                     a[j+1]=a[j];
+                else
+                    break;
     
             a[j+1]=val;
         }
